0b7853227e3c9b6cc3934e24bfbcc7675ab37ca9
stKhaDgar <33667544+stKhaDgar@users.noreply.github.com>
2017-11-23 22:13:15 +0200

    Create a project with form C++ (CLR)

---
+++ Sims/SimsForm.cpp
@@ -0,0 +1,11 @@
+#include "SimsForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Sims::SimsForm form;
+	Application::Run(%form);
+}
